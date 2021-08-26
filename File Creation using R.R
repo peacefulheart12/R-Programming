5f685710fcbe28a57eb550a9b4f63327a4e62939
Gaurav Tiwari <tiwarigaurav8642@gmail.com>
2021-08-26 17:45:57 +0200

    Add files via upload
    file creation

---
+++ File Creation using R.R
@@ -0,0 +1,14 @@
+#here is an example of variable
+first_variable <- "This is my variable"
+second_variable<- 12.5
+vec_1<-c(13,48.5,71,101.5,2)#making vectors 
+vec_1
+ToothGrowth %>% filter(dose==0.5)%>% arrange(len)#pipes of the R
+today()
+data.frame(x = c(1, 2, 3) , y = c(1.5, 5.5, 7.5))
+file.create (“new_text_file.txt”)
+
+
+file.create (“new_word_file.docx”) 
+
+file.create (“new_csv_file.csv”) 
